7b8a02ca5d94eef98c14d1cbe38135dea47fffb3
Fawaz <fawaz.chabi@imsp-uac.org>
2022-02-05 10:54:07 +0100

    sss comit

---
+++ db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+var pgp = require("pg-promise")(/*options*/);
+var username=process.env.USERNAME_DB, 
+    password=process.env.PASSWORD_DB,
+    port=process.env.PORT_DB,
+    host=process.env.HOST,
+    namedb=process.env.NAME_DB;
+var db = pgp("postgres://"+username+":"+password+"@"+host+":"+port+"/"+namedb);
+
+
+
+
+// Exporting the database object for shared use:
+module.exports = db;
