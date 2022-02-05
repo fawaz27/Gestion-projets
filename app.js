7577f1fa30ab5b1319d5b5c29064102a3d979d29
Fawaz <fawaz.chabi@imsp-uac.org>
2022-02-05 10:30:05 +0100

    s commit

---
+++ app.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env nodemon
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+var cookieSession = require('cookie-session');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const bodyParser =require("body-parser");
+const morgan     = require("morgan");
+const cors = require('cors');
+
+
+//Routes
+const usersRouter = require('../server/routes/users');
+const partnersRouter = require('../server/routes/partners');
+const projectsRouter = require('../server/routes/projects');
+const tasksRouter = require('../server/routes/tasks');
+
+
+//Moteur de template 
+app.set('view engine','ejs');
+
+app.use(express.static('/home/chabi/Documents/project/server/views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//Middleware
+//app.use(cookieSession({keys: ['key1', 'key2']}));
+app.use(session({
+  secret: 'thatsecretthinggoeshere',
+  resave: false,
+  saveUninitialized: true,
+  
+}));
+
+
+
+app.use(morgan('dev'));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use(function(req, res, next){
+  res.locals.message = req.flash('message');
+  next();
+});
+
+app.use('/partners', usersRouter);
+app.use('/', projectsRouter);
+//app.use('/partners', partnersRouter);
+
+require('./config/passport')(passport)
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening at http://${hostname}:${port}`)
+  });
